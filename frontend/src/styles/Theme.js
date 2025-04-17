6a25e98d478da56d19345fc6096a4cd92f62e063
MinjinChae <eominjin25@gmail.com>
2025-04-17 11:15:03 +0900

    init: 폰트 파일 추가 및 global style 설정

---
+++ frontend/src/styles/Theme.js
@@ -0,0 +1,29 @@
+const theme = {
+  colors: {
+    // 배경, 텍스트 - 흰색
+    white: "#FFFFFF",
+    // 텍스트 - 검은색
+    black: "#000000",
+    // footer - 회색
+    gray: "#E1E1E1",
+    // pink와의 조합 -회색 
+    bgGray: "#9DA09D",
+    // Navbar - 연한 핑크
+    bgPink: "#FFB1B2",
+    // 텍스트, 포인트 - 진한 핑크
+    pointPink: "#FC5BA8",
+  },
+
+  fonts: {
+    font: "'Spoqa Han Sans Neo', sans-serif",
+    weight: {
+      thin: 100,
+      light: 300,
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    }
+  },
+}
+
+export default theme
